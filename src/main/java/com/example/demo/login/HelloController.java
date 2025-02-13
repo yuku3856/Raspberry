351ef16afd92a3592e51fc05497993131bb48a7d
yuku <mie515173@gmail.com>
2025-02-13 10:11:14 +0900

    Thymeleaf導入

--- src/main/java/com/example/demo/login/HelloController.java
+++ src/main/java/com/example/demo/login/HelloController.java
@@ -1,14 +1,13 @@
 package com.example.demo.login;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 	
-	@GetMapping("/hello")
-	public String helloWorld() {
-		return "Hello Spring-Boot World!";
-
+	@GetMapping("/accountlist")
+	public String accountList() {
+		return "account/accountlist";
 	}
 }
