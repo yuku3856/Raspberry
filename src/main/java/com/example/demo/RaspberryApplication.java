755e4660f0a26aa17f850e81244e7319cbc9e836
yuku <mie515173@gmail.com>
2025-03-06 09:23:07 +0900

    別プロジェクト「Raspberry-DB」のプロジェクトをDB操作用のプロジェクトとして連携

--- src/main/java/com/example/demo/RaspberryApplication.java
+++ src/main/java/com/example/demo/RaspberryApplication.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.Controller.Sample;
+
 @SpringBootApplication
 public class RaspberryApplication {
 
 	public static void main(String[] args) {
+		Sample sample = new Sample();
+		System.out.println(sample.getMessage());
 		SpringApplication.run(RaspberryApplication.class, args);
 	}
-
+	
+//	public SpringApplication commandLineRunner (Sample sample) {
+//		return System.out.println(sample.getMessage());
+//	}
 }
